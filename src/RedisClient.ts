36cca18276c17d61460145b883bf83829610869b
Stella Widyasari <stellaw@thoughtworks.com>
2020-01-19 17:45:15 +0800

    Refactor dbclient and add test

---
+++ src/RedisClient.ts
@@ -0,0 +1,27 @@
+import redis from "redis";
+
+export class RedisClient {
+  private client: redis.RedisClient;
+
+  constructor(port: number, host: string) {
+    this.client = redis.createClient(port, host);
+  }
+
+  public connect = () => {
+    this.client.on("connect", () => {
+      console.log("Connected with redis");
+    });
+  }
+
+  public incrementByOne = (key: string) => {
+    this.client.incr(`${key}`);
+  }
+
+  public setKey = (key: string, value: string) => {
+    this.client.set(`${key}`, `${value}`);
+  }
+
+  public getValue = async (key: string, func: any) => {
+    this.client.get(`${key}`, func);
+  }
+}
--- src/__tests__/appTest.ts
+++ src/__tests__/appTest.ts
@@ -1,12 +1,28 @@
+import redis from "redis-mock";
 import request from "supertest";
-import { app } from "../app";
+import { App } from "../App";
 
-describe("app", () => {
+describe("App", () => {
+  let client;
+
   it("should show hello world", async () => {
-    const response = await request(app)
+    client = await redis.createClient({
+      port: 9000,
+    });
+    const response = await request(App)
       .get("/").expect(200)
       .expect("Content-Type", "text/html; charset=utf-8");
-
     expect(response.text).toEqual("Hello world");
+  });
+
+  it("should connect to redis", async () => {
+    await request(App).get("/");
+    client = await redis.createClient({
+      port: 9000,
+    });
+    await client.set("number_of_request", "4");
+    await client.get("number_of_request", async (_, result: string) => {
+      expect(result).toEqual("4");
+    });
   });
 });
